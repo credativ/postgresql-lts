ba89e4bbd50733a0dfbcadd6c56fe1469dca2560
Tatsuo Ishii <ishii@postgresql.org>
2014-12-30 19:37:55 +0900

    Fix resource leak pointed out by Coverity.

--- contrib/pgbench/pgbench.c
+++ contrib/pgbench/pgbench.c
@@ -1172,12 +1172,16 @@ parseQuery(Command *cmd, const char *raw_sql)
 		if (cmd->argc >= MAX_ARGS)
 		{
 			fprintf(stderr, "statement has too many arguments (maximum is %d): %s\n", MAX_ARGS - 1, raw_sql);
+			free(name);
 			return false;
 		}
 
 		sprintf(var, "$%d", cmd->argc);
 		if ((p = replaceVariable(&sql, p, eaten, var)) == NULL)
+		{
+			free(name);
 			return false;
+		}
 
 		cmd->argv[cmd->argc] = name;
 		cmd->argc++;
@@ -1303,7 +1307,10 @@ process_commands(char *buf)
 			case QUERY_EXTENDED:
 			case QUERY_PREPARED:
 				if (!parseQuery(my_commands, p))
+				{
+					free(my_commands);
 					return NULL;
+				}
 				break;
 			default:
 				return NULL;
@@ -1359,6 +1366,7 @@ process_file(char *filename)
 			commands = process_commands(&buf[i]);
 			if (commands == NULL)
 			{
+				free(my_commands);
 				fclose(fd);
 				return false;
 			}
@@ -1429,6 +1437,7 @@ process_builtin(char *tb)
 		commands = process_commands(buf);
 		if (commands == NULL)
 		{
+			free(my_commands);
 			return NULL;
 		}
 
