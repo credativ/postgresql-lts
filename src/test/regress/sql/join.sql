fdd8b774891859386c2fea9a2e01d4f0c7e9ea6b
Tom Lane <tgl@sss.pgh.pa.us>
2015-08-01 20:57:41 -0400

    Fix some planner issues with degenerate outer join clauses.
    An outer join clause that didn't actually reference the RHS (perhaps only
    after constant-folding) could confuse the join order enforcement logic,
    leading to wrong query results.  Also, nested occurrences of such things
    could trigger an Assertion that on reflection seems incorrect.
    
    Per fuzz testing by Andreas Seltenreich.  The practical use of such cases
    seems thin enough that it's not too surprising we've not heard field
    reports about it.
    
    This has been broken for a long time, so back-patch to all active branches.

--- src/test/regress/sql/join.sql
+++ src/test/regress/sql/join.sql
@@ -687,6 +687,31 @@ left join
 using (join_key);
 
 --
+-- test successful handling of nested outer joins with degenerate join quals
+--
+
+select t1.* from
+  text_tbl t1
+  left join (select *, '***'::text as d1 from int8_tbl i8b1) b1
+    left join int8_tbl i8
+      left join (select *, null::int as d2 from int8_tbl i8b2) b2
+      on (i8.q1 = b2.q1)
+    on (b2.d2 = b1.q2)
+  on (t1.f1 = b1.d1)
+  left join int4_tbl i4
+  on (i8.q2 = i4.f1);
+
+select t1.* from
+  text_tbl t1
+  left join (select *, '***'::text as d1 from int8_tbl i8b1) b1
+    left join int8_tbl i8
+      left join (select *, null::int as d2 from int8_tbl i8b2, int4_tbl i4b2) b2
+      on (i8.q1 = b2.q1)
+    on (b2.d2 = b1.q2)
+  on (t1.f1 = b1.d1)
+  left join int4_tbl i4
+  on (i8.q2 = i4.f1);
+
 -- test that quals attached to an outer join have correct semantics,
 -- specifically that they don't re-use expressions computed below the join;
 -- we force a mergejoin so that coalesce(b.q1, 1) appears as a join input
